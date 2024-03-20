9443faae5cf5a0fc1e9fb7f7c8cb1d184d803928
urbanoURN <edinsonpapu15@gmail.com>
2024-03-20 00:12:08 -0500

    v 0.3 Graficos

--- routes/graficos.py
+++ routes/graficos.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template
 import matplotlib.pyplot as plt
 import io
+import numpy as np
 from conexion import *
 
 
@@ -32,9 +33,37 @@ def grafConsu():
 @app.route('/grafTrac')
 def grafTrac():
 
-    # cursor = mysql.cursor()
-    # sql = "SELECT nombre, cantidad, fechasalida FROM servicios  "\
-    #       "INNER JOIN "
+    cursor = mysql.cursor()
+    sql = "SELECT tractores.marca, servicios.cantidad, servicios.fechasalida, COUNT(*) FROM servicios  "\
+          "INNER JOIN tractores ON servicios.idobjeto = tractores.idobjeto "\
+          "GROUP BY tractores.marca ORDER BY tractores.marca ASC"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    mysql.close()
+
+    # fig, ax = plt.subplots(figsize=(5, 3), layout='constrained')
+    # np.random.seed(19680801)
+
+    x = [dato[0] for dato in data]
+    y = [dato[1] for dato in data]
+    z = [dato[2] for dato in data]
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # linesy = ax.plot(x, z, label='Tractor Ferguson')
+
+    ax.scatter(z, y, label='[FALTA ESTO]')
+
+    ax.set_xlabel('Fechas De Salida')
+    ax.set_ylabel('Cantidad De Peticiones')
+    ax.set_title('GRAFICO DE PETIONES MENSUAL')
+    ax.legend()
+
+    plt.show()
+
+    fig, ax =plt.subplot(figsize=(5, 3), layout='constrained')
+    np.random.seed(19680801)
+
 
 
     return redirect('/graficos')
