0c39213121959e33a9e8c1f92ab0d0bafbe20861
Harold2645 <supapito85@gmail.com>
2024-05-12 00:59:12 -0500

    v 3.1

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,45 +1,159 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => { 
+    const btns_n01 = document.querySelectorAll('.btn-n01');
+    const btns_n02 = document.querySelectorAll('.btn-n02');
+    
+    const menu_n02 = document.querySelectorAll('.menu-n02');
+    const menu_n03 = document.querySelectorAll('.menu-n03');
 
-    const botns_n01 = document.querySelectorAll('.btn-n01');
+    function mostrarNiveles(boton_activado) {
+        switch (boton_activado) {
+            case "btn_usuarios":
+                let menu_usuarios = document.getElementById('menu_usuarios');
+                if (menu_usuarios.style.display == "" || menu_usuarios.style.display == "none") {
+                    menu_n03.forEach(elemento_03 => {
+                        elemento_03.style.display = "none";
+                    });
+                    menu_n02.forEach(elemento_02 => {
+                        elemento_02.style.display = "none";
+                    });
+                    menu_usuarios.style.display = "block";
+                } else {
+                    menu_usuarios.style.display = "none";
+                }
+                break;
 
-    function mostrarNiveles (segundo_menu){
+            case "btn_tractores":
+                    let menu_tractores = document.getElementById('menu_tractores');
+                    if (menu_tractores.style.display == "" || menu_tractores.style.display == "none") {
+                        menu_n03.forEach(elemento_03 => {
+                            elemento_03.style.display = "none";
+                        });
+                        menu_n02.forEach(elemento_02 => {
+                            elemento_02.style.display = "none";
+                        });
+                        menu_tractores.style.display = "block";
+                    } else {
+                        menu_tractores.style.display = "none";
+                    }
+                    break;
 
-        if (segundo_menu == "btn_productos"){
+            case "btn_herramienta":
+                    let menu_herramienta = document.getElementById('menu_herramienta');
+                    if (menu_herramienta.style.display == "" || menu_herramienta.style.display == "none") {
+                        menu_n03.forEach(elemento_03 => {
+                            elemento_03.style.display = "none";
+                        });
+                        menu_n02.forEach(elemento_02 => {
+                            elemento_02.style.display = "none";
+                        });
+                        menu_herramienta.style.display = "block";
+                    } else {
+                        menu_herramienta.style.display = "none";
+                    }
+                    break;
 
-            let menu_prod = document.getElementById('menu_prod');
+                case "btn_categoria":
+                        let menu_categorias = document.getElementById('menu_categorias');
+                        if (menu_categorias.style.display == "" || menu_categorias.style.display == "none") {
+                            menu_n03.forEach(elemento_03 => {
+                                elemento_03.style.display = "none";
+                            });
+                            menu_n02.forEach(elemento_02 => {
+                                elemento_02.style.display = "none";
+                            });
+                            menu_categorias.style.display = "block";
+                        } else {
+                            menu_categorias.style.display = "none";
+                        }
+                        break;
 
-            if (menu_prod.style.display == "" || menu_prod.style.display == "none") {
-                menu_prod.style.display = "block";
-            } else {
-                menu_prod.style.display = "none";
-            }
-        }
+                case "btn_novedad":
+                    let menu_novedad = document.getElementById('menu_novedad');
+                    if (menu_novedad.style.display == "" || menu_novedad.style.display == "none") {
+                        menu_n03.forEach(elemento_03 => {
+                            elemento_03.style.display = "none";
+                        });
+                        menu_n02.forEach(elemento_02 => {
+                            elemento_02.style.display = "none";
+                        });
+                        menu_novedad.style.display = "block";
+                    } else {
+                        menu_novedad.style.display = "none";
+                    }
+                    break;
 
-        if (segundo_menu == "btn_servicios"){
+                    case "btn_servicios":
+                        let menu_servicios = document.getElementById('menu_servicios');
+                        if (menu_servicios.style.display == "" || menu_servicios.style.display == "none") {
+                            menu_n03.forEach(elemento_03 => {
+                                elemento_03.style.display = "none";
+                            });
+                            menu_n02.forEach(elemento_02 => {
+                                elemento_02.style.display = "none";
+                            });
+                            menu_servicios.style.display = "block";
+                        } else {
+                            menu_servicios.style.display = "none";
+                        }
+                        break;
 
-            let menu_serv = document.getElementById('menu_serv');
 
-            if (menu_serv.style.display == "" || menu_serv.style.display == "none") {
-                menu_serv.style.display = "block";
-            }else{
-                menu_serv.style.display = "none";
-            }
+                    case "btn_insumo":
+                        let menu_insumo = document.getElementById('menu_insumo');
+                        if (menu_insumo.style.display == "" || menu_insumo.style.display == "none") {
+                            menu_n03.forEach(elemento_03 => {
+                                elemento_03.style.display = "none";
+                            });
+                            menu_n02.forEach(elemento_02 => {
+                                elemento_02.style.display = "none";
+                            });
+                            menu_insumo.style.display = "block";
+                        } else {
+                            menu_insumo.style.display = "none";
+                        }
+                        break;
+                        
+                    case "btn_liquido":
+                        let menu_liquidos = document.getElementById('menu_liquidos');
+                        if (menu_liquidos.style.display == "" || menu_liquidos.style.display == "none") {
+                            menu_n03.forEach(elemento_03 => {
+                                elemento_03.style.display = "none";
+                            });
+                            menu_liquidos.style.display = "block";
+                        } else {
+                            menu_liquidos.style.display = "none";
+                        }
+                        break;
+
+            default:
+                break;
         }
     }
 
-    botns_n01.forEach(boton => {
+    btns_n01.forEach(boton => {
         boton.addEventListener('click', (objeto) => {
-            console.log(objeto.target.id)
-
-            if (objeto.target.id == "btn_productos"){
-                mostrarNiveles (objeto.target.id);
-            }
-
-            if (objeto.target.id == "btn_servicios"){
-                mostrarNiveles (objeto.target.id);
-            }
+            mostrarNiveles(objeto.target.id);
         });
     });
+
+    btns_n02.forEach(boton => {
+        boton.addEventListener('click', (objeto) => {
+            mostrarNiveles(objeto.target.id);
+        });
+    });
+
+
+const nav = document.querySelector("#menu_cont");
+const abrir = document.querySelector("#disparador");
+const cerrar = document.querySelector("#close")
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
     
 
-})
+});
