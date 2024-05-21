bb083afb6324e7935dfd6f5e195c65d67200a90c
urbanoURN <edinsonpapu15@gmail.com>
2024-05-21 07:56:16 -0500

    V 1.12

--- routes/enviosConsu.py
+++ routes/enviosConsu.py
@@ -4,6 +4,8 @@ from email.message import EmailMessage
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from flask import redirect, render_template
 from models.enviosConsu import misEnvios
 from routes.graficos import *
@@ -14,38 +16,51 @@ def envioConsuPez():
 
     data = misEnvios.datoacpm()
 
-    grafico = grafConsu()
+    grafConsu()
+#    grafico.show()
 
-    grafico = io.BytesIO()
-    plt.savefig(grafico, format='png')
-    grafico.seek(0)
+    #grafico = io.BytesIO()
+    # grafico.savefig("grafico.pdf")
+#    grafico.seek(0)
 
-    imgrafico = base64.b64encode(grafico.read()).decode('utf-8')
+    #imgrafico = base64.b64encode(grafico.read()).decode('utf-8')
 
-    if data and data[0][0] <= 25:
-        remitente = "hangarsena24@outlook.com"
-        destinatario = "padilla2645@gmail.com"
-        mensaje = "Aqui se muestra la cantidad de conbusible que hay bodega solicitamos mas"
+    # if data and data[0][0] <= 25:
+    remitente = "hangarsena24@outlook.com"
+    destinatario = "padilla2645@gmail.com"
+    mensaje = "Aqui se muestra la cantidad de conbusible que hay bodega solicitamos mas"
 
-        email = MIMEMultipart()
-        email["From"] = remitente
-        email["To"] = destinatario
-        email["subject"] = "correo de prueba"
+    email = MIMEMultipart()
+    email["From"] = remitente
+    email["To"] = destinatario
+    email["subject"] = "correo de prueba"
 
-        body = MIMEText(mensaje)
-        email.attach(body)
+    body = MIMEText(mensaje)
+    email.attach(body)
+    filename = "grafico.pdf"
+    attachment = open(filename, "rb")
+    # The instance of MIMEBase and named as p
+    p = MIMEBase("application", "octet-stream")
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+    # encode into base64
+    encoders.encode_base64(p)
+    p.add_header("Content-Disposition", "attachment; filename= %s" % filename)
+    # attach the instance 'p' to instance 'msg'
+    email.attach(p)
 
-        img = MIMEImage(base64.b64decode(imgrafico), name='grafico.png')
-        email.attach(img)
 
-        smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
-        smtp.starttls()
-        smtp.login(remitente,"correosena03")
-        smtp.sendmail(remitente, destinatario, email.as_string())
-        smtp.quit()
-        return redirect('/graficos')
-    else:
-        return redirect('/funciones')
+    #img = MIMEImage(base64.b64decode(imgrafico), name='grafico.png')
+    #email.attach(p)
+
+    smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+    smtp.starttls()
+    smtp.login(remitente,"correosena03")
+    smtp.sendmail(remitente, destinatario, email.as_string())
+    smtp.quit()
+    return redirect('/graficos')
+    # else:
+    #     return redirect('/funciones')
 
  
 
--- routes/graficos.py
+++ routes/graficos.py
@@ -23,33 +23,33 @@ def grafConsu():
     ax.bar(x, y)
     ax.set_xlabel('Tipos')
     ax.set_ylabel('Cantidad en Bodega')
-    plt.show()
-    
-    return redirect('/graficos')
+    plt.savefig("grafico.pdf")
+#    return redirect('/graficos')
 
 @app.route('/grafTrac')
 def grafTrac():
 
     data = misGraficos.datosTractores()
 
+    x = [dato[1] for dato in data] #fecha
+    y = [dato[0] for dato in data] #cantidad
+    nombreT = [dato[2] for dato in data] #nombre
+
+    cantidad_tractores = len(nombreT)
+
     fig, ax = plt.subplots(figsize=(8, 6))
 
-    tractores_vistor = set()
+    # tractores_vistor = set()
 
-    for dato in data:
-        x = dato[1] #fecha
-        y = dato[0] #cantidad
-        nombreT = dato[2] #nombre
+    for i in range(cantidad_tractores):
         
-        if nombreT not in tractores_vistor:
-
-            x_trac = [dato[1] for dato in data if dato[2] == nombreT]
-            y_trac = [dato[0] for dato in data if dato[2] == nombreT]  
-            
-            lineas = ax.plot(x_trac, y_trac, label=nombreT)
-            tractores_vistor.add(nombreT)
-
-
+        # x_trac = [dato[1] for dato in data if dato[2] == nombreT]
+        x_trac = list(range(1, y[i] + 1))
+        # y_trac = [dato[0] for dato in data if dato[2] == nombreT]  
+        y_trac = [nombreT[i]] * y[i]
+        
+        ax.plot(x_trac, y_trac, marker="o", label=nombreT[i])
+       
     ax.set_xlabel('Fechas')
     ax.set_ylabel('Cantidad de Peticiones')
     ax.set_title('Solicitudes de Tractores')
@@ -58,3 +58,31 @@ def grafTrac():
     plt.show()
 
     return redirect('/graficos')
+
+
+# data = misGraficos.datosTractores()
+
+#     fig, ax = plt.subplots(figsize=(8, 6))
+
+#     tractores_vistor = set()
+
+#     for dato in data:
+#         x = dato[1] #fecha
+#         y = dato[0] #cantidad
+#         nombreT = dato[2] #nombre
+        
+#         if nombreT not in tractores_vistor:
+
+#             x_trac = [dato[1] for dato in data if dato[2] == nombreT]
+#             y_trac = [dato[0] for dato in data if dato[2] == nombreT]  
+            
+#             lineas = ax.plot(x_trac, y_trac, label=nombreT)
+#             tractores_vistor.add(nombreT)
+
+
+#     ax.set_xlabel('Fechas')
+#     ax.set_ylabel('Cantidad de Peticiones')
+#     ax.set_title('Solicitudes de Tractores')
+#     ax.legend()
+
+#     plt.show()
