8fe70a9998e7cf9e89891481f4a4732aa159d77f
urbanoURN <edinsonpapu15@gmail.com>
2024-06-04 00:44:52 -0500

    urbano cambio 0.4

--- principal.py
+++ principal.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_from_directory, session
+from flask import render_template, send_from_directory, session, url_for
 from conexion import *
 from routes.usuarios import *
 from routes.novedades import *
@@ -21,6 +21,11 @@ def uploads(nombre):
 def index():
     session["logueado"] = False
     return render_template('/index.html') 
+
+@app.errorhandler(404)
+def not_found(error):
+    # return redirect(url_for('404.html'))
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port="5080")
