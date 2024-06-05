e5483345ac4371d44049d3ca1fa74541d8abbb8d
Harold2645 <supapito85@gmail.com>
2024-06-05 08:20:12 -0500

    v 4

--- principal.py
+++ principal.py
@@ -23,10 +23,10 @@ def index():
     session["logueado"] = False
     return render_template('/index.html') 
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     # return redirect(url_for('404.html'))
-#     return render_template('404.html'), 404
+@app.errorhandler(404)
+def not_found(error):
+    # return redirect(url_for('404.html'))
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port="5080")
--- prueba_json.py
+++ prueba_json.py
@@ -1,5 +1,6 @@
-from flask import request
+from flask import jsonify, request
 from conexion import *
+
 
 @app.route("/peticion")
 def peticion():
@@ -10,15 +11,57 @@ def peticion():
 
 @app.route("/peticion_forma", methods=['POST'])
 def peticion_forma():
-    documento = request.form['usuario']
+    # documento = requests.post['usuario']
+    documento = request.get_json().get('usuario')
 
     sql = f"SELECT rol FROM usuarios WHERE documento='{documento}'"
     cur = conexion.cursor()
     cur.execute(sql)
     resultado = cur.fetchone()
-
     respuesta = {
         "Rol": resultado
     }
+    return respuesta   
 
-    return respuesta
+@app.route("/peticion_prueba", methods=['POST'])
+def peticion_prueba():
+    documento = request.get_json().get('usuario')
+
+    if len(documento) >= 6 & len(documento) <= 15:
+        sql = f"SELECT rol FROM usuarios WHERE documento='{documento}'"
+        cur = conexion.cursor()
+        cur.execute(sql)
+        resultado = cur.fetchall()
+
+        microrespuesta = {
+            'valor': resultado
+        }
+
+        return microrespuesta
+
+    else: 
+        microrespuesta = {"valor:" 'NO'}
+        return microrespuesta
+
+
+
+
+@app.route('/peticion_forma_prueba', methods=['POST'])
+def promedio(cedula):
+    try:
+        connection = conexion()
+        cursor = connection.cursor()
+        cursor.execute (f"SELECT ROUND(AVG(puntaje)) AS promedio FROM calificacion WHERE idcontratista = {cedula}")
+        column_names = [column[0] for column in cursor.description]
+        datos = cursor.fetchall()
+        if(len(datos)==0):
+            cursor.close()
+            connection.close()
+            return jsonify({"msg":"notFound"})
+        else:
+            cursor.close()
+            connection.close()
+            return jsonify([dict(zip(column_names, dato)) for dato in datos])
+    except Exception as e:
+        return jsonify({"error": str(e)})  
+
