1979e2372f4e8bab6a1931ffa5cdbea834e65165
Alexis <guelesix@hotmail.com>
2024-07-30 09:24:33 -0500

    Ionic

--- ionic.py
+++ ionic.py
@@ -1,5 +1,37 @@
 from conexion import *
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from models.usuarios import misUsuarios
+import json
 
 
+@app.route('/consultaUsuario', methods=['GET'])
+def consulta_supervisor():
+    try:
+        connection = conexion
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM usuarios WHERE activo=1")
+        column_names = [column[0] for column in cursor.description]
+        datos = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return jsonify([dict(zip(column_names, dato)) for dato in datos])
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    # res = misUsuarios.consultar()
+    # return json.dump(res)
+
+
+@app.route('/registrarUsuario', methods=['POST'])
+def inserta_cajero():
+    try:
+        data = request.get_json()
+        connection = conexion
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO usuario (documento, nombre, apellido, celular) VALUES (%s, %s, %s, %s)", (data['documento'], data['nombre'], data['apellido'], data['celular']))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return jsonify({"msg": 'ok'})
+    except Exception as e:
+        return jsonify({"error": str(e)})  
