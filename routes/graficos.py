8bd71f3817734fa07b5287833df96807a344d3f9
urbanoURN <edinsonpapu15@gmail.com>
2024-09-03 12:07:57 -0500

    cambio foto fondo

--- routes/graficos.py
+++ routes/graficos.py
@@ -39,7 +39,6 @@ def guardagrafico():
             species = [dato[0] for dato in data]
             existing = [dato[1] for dato in data]
 
-            # Asumiendo cantidades máximas y mínimas genéricas para cada consumible
             max_qty = [100] * len(species)  # Cantidad máxima por defecto
             min_qty = [20] * len(species)   # Cantidad mínima por defecto
 
@@ -96,6 +95,41 @@ def guardagrafico():
 
 @app.route('/grafConsu')
 def grafConsu():
+
+
+#     # data from https://allisonhorst.github.io/palmerpenguins/
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# species = ("Adelie", "Chinstrap", "Gentoo")
+# penguin_means = {
+#     'Bill Depth': (18.35, 18.43, 14.98),
+#     'Bill Length': (38.79, 48.83, 47.50),
+#     'Flipper Length': (189.95, 195.82, 217.19),
+# }
+
+# x = np.arange(len(species))  # the label locations
+# width = 0.25  # the width of the bars
+# multiplier = 0
+
+# fig, ax = plt.subplots(layout='constrained')
+
+# for attribute, measurement in penguin_means.items():
+#     offset = width * multiplier
+#     rects = ax.bar(x + offset, measurement, width, label=attribute)
+#     ax.bar_label(rects, padding=3)
+#     multiplier += 1
+
+# # Add some text for labels, title and custom x-axis tick labels, etc.
+# ax.set_ylabel('Length (mm)')
+# ax.set_title('Penguin attributes by species')
+# ax.set_xticks(x + width, species)
+# ax.legend(loc='upper left', ncols=3)
+# ax.set_ylim(0, 250)
+
+# plt.show()
+
     if session.get("loginCorrecto"):
         rol = session['rol'] 
         nombre = session['nombreUsuario']
@@ -105,7 +139,7 @@ def grafConsu():
             species = [dato[0] for dato in data]
             existing = [dato[1] for dato in data]
 
-            # Asumiendo cantidades máximas y mínimas genéricas para cada consumible
+            
             max_qty = [100] * len(species)  # Cantidad máxima por defecto
             min_qty = [20] * len(species)   # Cantidad mínima por defecto
 
